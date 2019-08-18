6fa0107234ec8793372b5b498d0c7a75daafe14e
Paul Goddard <48760536+pgoddard10@users.noreply.github.com>
2019-08-18 18:19:18 +0100

    Added more comments

--- Capacitive_Sensor_LED.ino
+++ Capacitive_Sensor_LED.ino
@@ -1,23 +1,24 @@
-#include <CapacitiveSensor.h>
+#include <CapacitiveSensor.h> //library from https://github.com/PaulStoffregen/CapacitiveSensor
 
 CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8); // 1M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
 
 void setup()                    
 {
    cs_4_8.set_CS_AutocaL_Millis(0xFFFFFFFF);// turn off autocalibrate on channel 1 - just as an example
-   Serial.begin(9600);
-   pinMode(7,OUTPUT);
+   Serial.begin(9600); //enable output to the console
+   pinMode(7,OUTPUT); //enable pin 7
 }
 
 void loop() {
-  long sensor1 =  cs_4_8.capacitiveSensor(50);
+  long sensor1 =  cs_4_8.capacitiveSensor(50); //get reading from capacitive touch pin
   
-  if(sensor1>=40) Serial.println(sensor1);  // print sensor output
+  if(sensor1>=40) //threshold level
+     Serial.println(sensor1);  // print sensor output
   
-  if(sensor1 >= 60) {
-    digitalWrite(7,HIGH);
+  if(sensor1 >= 60) { //threshold level
+    digitalWrite(7,HIGH); //push signal over pin 7. e.g. turn LED on
   }
   else{
-    digitalWrite(7,LOW);
+    digitalWrite(7,LOW); //turn signal off. e.g. turn LED off
   } 
 }
